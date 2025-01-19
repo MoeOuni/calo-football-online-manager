cedb0f6aa9d8d32535fe6e2b5e98a1a1dd9fd77c
Moe Ouni <moe.ouni20@gmail.com>
2025-01-19 21:05:35 +0100

    remove unused configuration and environment files from API

--- api/src/database/index.ts
+++ api/src/database/index.ts
@@ -6,7 +6,7 @@ export const dbConnection = async () => {
     url: `mongodb://${DB_HOST}:${DB_PORT}/${DB_DATABASE}`,
     options: {
       useNewUrlParser: true,
-      useUnifiedTopology: true
+      useUnifiedTopology: true,
     },
   };
 
