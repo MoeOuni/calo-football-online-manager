f36b3ac16f7a7b2cbe8214f8d125b9c97fcae41b
Moe Ouni <moe.ouni20@gmail.com>
2025-01-19 21:23:17 +0100

    feat: add suspense fallback loader to main application render

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,9 +2,19 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { ReactQueryProvider } from "./providers/react-query-provider.tsx";
+import { Suspense } from "react";
+import { LoaderCircle } from "lucide-react";
 
 createRoot(document.getElementById("root")!).render(
+  <Suspense
+    fallback={
+      <div className="flex min-h-[100vh] items-center justify-center">
+        <LoaderCircle className="mr-2 h-4 w-4 animate-spin" />
+      </div>
+    }
+  >
   <ReactQueryProvider>
     <App />
   </ReactQueryProvider>
+  </Suspense>
 );
