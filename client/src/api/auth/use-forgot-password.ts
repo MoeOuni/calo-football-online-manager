2f18c712c2f4a31e343472a2cb7f4f3fbbc7a1ec
Moe Ouni <moe.ouni20@gmail.com>
2025-01-20 23:13:58 +0100

    feat: implement forgot password functionality with error handling and query invalidation

--- client/src/api/auth/use-forgot-password.ts
+++ client/src/api/auth/use-forgot-password.ts
@@ -1,22 +1,28 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { apiClient, authQueryKeys } from "@/api";
 import { TSFixMe, IForgotPwd } from "@/lib/interfaces";
+import { toast } from "sonner";
 
 export function useForgotPwd() {
   const queryClient = useQueryClient();
 
   const forgotPwdFn = async (payload: IForgotPwd) => {
-    // Todo
-    return null;
+    const response = await apiClient.post("/forgot-password", { payload });
+
+    return response.data;
   };
 
   return useMutation({
     mutationFn: forgotPwdFn,
     onSuccess: () => {
-      // Todo
+      queryClient.invalidateQueries({
+        queryKey: authQueryKeys.details(),
+      });
     },
     onError: (error: TSFixMe) => {
-      // Todo
+      toast.error(
+        error?.response?.data?.message || "An error occurred. Please try again."
+      );
     },
   });
 }
