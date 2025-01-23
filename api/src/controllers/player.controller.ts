f0b547cf26d9c8325abfdcd4226371bca1fe4792
Moe Ouni <moe.ouni20@gmail.com>
2025-01-23 20:30:15 +0100

    feat: implement player market functionality with listing and purchasing options and implement log module

---
+++ api/src/controllers/player.controller.ts
@@ -0,0 +1,42 @@
+import { NextFunction, Response } from 'express';
+import { Container } from 'typedi';
+import { RequestWithUser } from '@interfaces/auth.interface';
+import { PlayerService } from '@/services/player.service';
+import { IPlayer } from '@/interfaces/player.interface';
+import { stat } from 'fs';
+
+export class PlayerController {
+  public player = Container.get(PlayerService);
+
+  public listPlayerToSell = async (req: RequestWithUser, res: Response, next: NextFunction) => {
+    try {
+      const { playerId, price } = req.body;
+
+      const player = await this.player.listPlayerToSell(playerId, price, req.user);
+
+      res.status(201).json({
+        data: player,
+        status: 'success',
+        message: `${player.name} has been set to sale 🎉`,
+      });
+    } catch (error) {
+      next(error);
+    }
+  };
+
+  public removePlayerFromMarket = async (req: RequestWithUser, res: Response, next: NextFunction) => {
+    try {
+      const { playerId } = req.body;
+
+      const player = await this.player.removePlayerFromMarket(playerId, req.user);
+
+      res.status(201).json({
+        data: player,
+        status: 'success',
+        message: `${player.name} has been removed from market 🎉`,
+      });
+    } catch (error) {
+      next(error);
+    }
+  };
+}
