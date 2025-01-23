76969c98e32820434012a38fd4d5d917dac64523
Moe Ouni <moe.ouni20@gmail.com>
2025-01-24 00:41:52 +0100

    feat: streamline player action menu by conditionally rendering sale options

--- client/src/components/data-tables/players-action-menu.tsx
+++ client/src/components/data-tables/players-action-menu.tsx
@@ -41,30 +41,29 @@ const PlayersActionMenu = ({ record }: { record: IPlayerPopulated }) => {
         <DropdownMenuContent className="w-54">
           <DropdownMenuLabel>Player Actions</DropdownMenuLabel>
           <DropdownMenuSeparator />
+          {!record?.upToSale ? (
+            <DropdownMenuItem onSelect={(e) => e.preventDefault()}>
+              <div
+                onClick={handleListForSale}
+                className="flex items-center gap-2"
+              >
+                <Tag className="h-4 w-4" />
+                <span>List for Sale</span>
+              </div>
+            </DropdownMenuItem>
+          ) : (
+            <DropdownMenuItem onSelect={(e) => e.preventDefault()}>
+              <div
+                onClick={handleRemovePlayerFromMarket}
+                className="flex items-center gap-2"
+              >
+                <XCircle className="h-4 w-4" />
+                <span>Remove from Market</span>
+              </div>
+            </DropdownMenuItem>
+          )}
           <DropdownMenuItem onSelect={(e) => e.preventDefault()}>
-            <div
-              onClick={handleListForSale}
-              className="flex items-center gap-2"
-            >
-              <Tag className="h-4 w-4" />
-              <span>List for Sale</span>
-            </div>
-          </DropdownMenuItem>
-
-          <DropdownMenuItem onSelect={(e) => e.preventDefault()}>
-            <div
-              onClick={handleRemovePlayerFromMarket}
-              className="flex items-center gap-2"
-            >
-              <XCircle className="h-4 w-4" />
-              <span>Remove from Market</span>
-            </div>
-          </DropdownMenuItem>
-
-          <DropdownMenuItem onSelect={(e) => e.preventDefault()}>
-            <div
-              className="flex items-center gap-2"
-            >
+            <div className="flex items-center gap-2">
               <Users className="h-4 w-4" />
               Assign to Team
             </div>
